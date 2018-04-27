31f48978a1563db6e964992c4fb4fcdfb5deef83
Artem Orlov <artem_orlov1@epam.com>
2018-04-27 13:55:12 +0300

    Add view controller

---
+++ ImageProcessing/ImageProcessing/ImageProcessingViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ImageProcessingViewController.swift
+//  ImageProcessing
+//
+//  Created by Artem Orlov on 27/04/2018.
+//  Copyright © 2018 ArtemOrlov. All rights reserved.
+//
+
+import UIKit
+
+class ImageProcessingViewController: UIViewController {
+
+    @IBOutlet private weak var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
---
+++ ImageProcessing/ImageProcessing/Supporting Files/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  ImageProcessing
+//
+//  Created by Artem Orlov on 27/04/2018.
+//  Copyright © 2018 ArtemOrlov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+
+}
+
